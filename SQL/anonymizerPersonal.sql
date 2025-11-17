c2143f9e7ad53cd65cda48828746392dd3034040
Sandra2021211 <87116316+Sandra2021211@users.noreply.github.com>
2025-11-17 08:16:00 +0000

    added changes

--- SQL/anonymizerPersonal.sql
+++ SQL/anonymizerPersonal.sql
@@ -25,7 +25,7 @@ END;
 UPDATE personal
 SET dob = CASE
     WHEN :column='dob' AND :technique='generalization' THEN
-        substr(dob, 4, 2) || '/' || substr(dob, 7, 4)
+    substr(dob, 6, 2) || '/' || substr(dob, 1, 4)
 
     WHEN :column='dob' AND :technique='suppression' THEN
         '[HIDDEN]'
@@ -57,7 +57,7 @@ END;
 UPDATE personal
 SET phone = CASE
     WHEN :column='phone' AND :technique='masking' THEN
-        substr(phone, 1, 7) || 'XXXXXX'
+        substr(phone, 1, 4) || 'XXXXXX'
 
     WHEN :column='phone' AND :technique='tokenization' THEN
         'PHN-TKN-' || abs(random() % 9000 + 1000)
